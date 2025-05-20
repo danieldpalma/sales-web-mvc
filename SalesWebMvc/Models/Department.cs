a60aaea684717ff3ae72357c016205cb3e6cab46
danieldpalma <danielpalma232@gmail.com>
2025-05-20 11:03:54 -0300

    Added method of Department model

--- SalesWebMvc/Models/Department.cs
+++ SalesWebMvc/Models/Department.cs
@@ -4,4 +4,19 @@ public class Department
 {
     public int Id { get; set; }
     public string Name { get; set; } = string.Empty;
+    public ICollection<Seller> Sellers { get; set; } = [];
+
+    public Department()
+    {
+    }
+
+    public Department(int id, string name)
+    {
+        Id = id;
+        Name = name;
+    }
+
+    public void AddSeler(Seller seller) => Sellers.Add(seller);
+
+    public double TotalSales(DateTime inital, DateTime final) => Sellers.Sum(seller => seller.TotalSales(inital, final));
 }
